0299a9f14a16f7bde122b66067053712d269488b
ARJUN-SABU <arjunsabu99@gmail.com>
2023-08-13 09:47:39 +0530

    Changed the api url in the frontend to the actual backend url for testing

--- frontend/src/components/ChatPage.js
+++ frontend/src/components/ChatPage.js
@@ -5,7 +5,14 @@ import { useParams } from "react-router-dom";
 function ChatPage() {
   const params = useParams();
   useEffect(() => {
-    fetch(`http://localhost:8000/user-chats-and-groups/${params.userId}`)
+    // fetch(`http://localhost:8000/user-chats-and-groups/${params.userId}`)
+    //   .then((data) => data.json())
+    //   .then((userDoc) => console.log(userDoc))
+    //   .catch((err) => console.log(err));
+
+    fetch(
+      `https://chat-spider-backend.vercel.app/user-chats-and-groups/${params.userId}`
+    )
       .then((data) => data.json())
       .then((userDoc) => console.log(userDoc))
       .catch((err) => console.log(err));
